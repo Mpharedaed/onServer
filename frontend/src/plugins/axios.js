7d46f1e202d11a1e6b027fcda5ea30774c5d8b91
Mohamed Reda <109115645+Mpharedaed@users.noreply.github.com>
2024-09-15 20:07:05 -0500

    link updated 8

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -2,18 +2,23 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://blogi-36jo.onrender.com',
+  baseURL: 'https://blogi-36jo.onrender.com/api/',
   headers: {
     'Content-Type': 'application/json'
   }
-});        
+});
+       
 
 instance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
+    console.log('Token found and added to headers:', token); // Add this log to debug
     config.headers.Authorization = `Bearer ${token}`;
+  } else {
+    console.log('No token found in localStorage');
   }
   return config;
 });
+
 
 export default instance;
