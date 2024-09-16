7000c3fd36b32c482f8435c78d84a95149f4bd7f
Mohamed Reda <109115645+Mpharedaed@users.noreply.github.com>
2024-09-15 20:12:44 -0500

    link updated 9

--- frontend/src/plugins/axios.js
+++ frontend/src/plugins/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://blogi-36jo.onrender.com/api/',
+  baseURL: 'https://blogi-36jo.onrender.com/api/auth',
   headers: {
     'Content-Type': 'application/json'
   }
